cf5f2a439518a23a2420bc8b4bfabc9c72b90ff8
Jajang-man-bungbung <91002280+RejikaParbosa@users.noreply.github.com>
2022-06-01 11:24:37 +0300

    To define if its a  positive or negetive number

---
+++ pos_neg.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int no1;
+	cout<<"Enter number:";
+	cin>> no1;
+	if(no1>=0)
+	{
+		cout<< "This is a positive number" <<no1 <<endl;
+	}
+	else
+	{
+		cout<< "This is a negetive number" <<no1 <<endl;
+	}
+	return 0;
+} 
